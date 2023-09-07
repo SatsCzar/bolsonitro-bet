bd0de54e848463e3e64ff28f27975b8a60b36c79
SatsCzar <104809510+SatsCzar@users.noreply.github.com>
2023-09-07 19:23:10 -0300

    Add step to credit amount and minor changes
    Should credit the deposit in the transaction table and expire the intentions whose BOLT11 expired in the network without being paid.

--- src/domain/usecases/checkDepositIntentions.js
+++ src/domain/usecases/checkDepositIntentions.js
@@ -1,12 +1,16 @@
-const { usecase, step, Ok } = require("@herbsjs/herbs")
+const { usecase, step, Ok, checker } = require("@herbsjs/herbs")
 const { herbarium } = require("@herbsjs/herbarium")
 const LightningClient = require("../../infra/clients/LightningClient")
 const DepositIntentionsRepository = require("../../infra/database/repositories/DepositIntentionsRepository")
+const TransactionRepository = require("../../infra/database/repositories/TransactionRepository")
 const depositStatusEnum = require("../enums/depositStatusEnum")
+const Transaction = require("../entities/transaction")
+const transactionTypesEnum = require("../enums/transactionTypesEnum")
 
 const dependency = {
   LightningClient,
   DepositIntentionsRepository,
+  TransactionRepository,
 }
 
 const checkDepositIntentions = (injection) =>
@@ -19,15 +23,20 @@ const checkDepositIntentions = (injection) =>
 
     setup: (ctx) => {
       ctx.di = Object.assign({}, dependency, injection)
-      ctx.di.depositIntentionsDatabase = new ctx.di.DepositIntentionsRepository()
+      ctx.di.depositIntentionsInstance = new ctx.di.DepositIntentionsRepository()
+      ctx.di.transactionRepositoryInstance = new ctx.di.TransactionRepository()
       ctx.di.lightningClient = new ctx.di.LightningClient()
-      ctx.data = {}
+      ctx.data = {
+        currentDate: ctx.di.currentDate || new Date(),
+      }
     },
 
     "Find all pending deposit intentions": step(async (ctx) => {
-      const { depositIntentionsDatabase } = ctx.di
+      const { depositIntentionsInstance } = ctx.di
 
-      const depositIntentionsPending = await depositIntentionsDatabase.findAllPending()
+      const depositIntentionsPending = await depositIntentionsInstance.findAllPending()
+
+      if (checker.isEmpty(depositIntentionsPending)) return Ok(ctx.stop())
 
       ctx.data.intentionsPending = depositIntentionsPending
 
@@ -53,58 +62,115 @@ const checkDepositIntentions = (injection) =>
       return Ok()
     }),
 
-    "Check the intentions that were paid": step((ctx) => {
-      const { invoices, intentionsPending } = ctx.data
+    "Check intentions paid and credit the amount in user account": step({
+      "Check the intentions that were paid": step((ctx) => {
+        const { invoices, intentionsPending } = ctx.data
 
-      const intentionsPaid = invoices
+        const intentionsPaid = invoices
+          .map((invoice) => {
+            const werePaid = invoice.is_confirmed
+
+            if (werePaid) return intentionsPending.find((intention) => intention.invoiceId === invoice.id)
+          })
+          .filter(Boolean)
+
+        ctx.data.intentionsPaid = intentionsPaid
+
+        return Ok()
+      }),
+
+      "Changes the status of intents that have been paid": step(async (ctx) => {
+        const { intentionsPaid } = ctx.data
+        const { depositIntentionsInstance } = ctx.di
+
+        const intentionsCredited = []
+
+        for (const intention of intentionsPaid) {
+          try {
+            intention.status = depositStatusEnum.credited
+
+            const updated = await depositIntentionsInstance.update(intention)
+
+            intentionsCredited.push(updated)
+          } catch (error) {
+            console.log(error)
+          }
+        }
+
+        ctx.data.intentionsCredited = intentionsCredited
+
+        return Ok()
+      }),
+
+      "Credit amount in user account": step(async (ctx) => {
+        const { transactionRepositoryInstance } = ctx.di
+        const { intentionsCredited, currentDate } = ctx.data
+
+        for (const intention of intentionsCredited) {
+          try {
+            const transaction = Transaction.fromJSON({
+              amount: intention.amount,
+              chatId: intention.chatId,
+              date: currentDate,
+              type: transactionTypesEnum.incoming,
+              intentionId: intention.id,
+            })
+
+            await transactionRepositoryInstance.insert(transaction)
+          } catch (error) {
+            console.log(error)
+          }
+        }
+        return Ok()
+      }),
+
+      "Notifies users that the deposit has been credited": step(async (ctx) => {
+        const { bot } = ctx.di
+        const { intentionsCredited } = ctx.data
+
+        for (const intention of intentionsCredited) {
+          try {
+            const message = `Your deposit of ${intention.amount} has been credited`
+
+            await bot.telegram.sendMessage(intention.chatId, message)
+          } catch (error) {
+            console.log(`Error while try to send message to user ${intention.chatId}: ${error.message}`)
+          }
+        }
+        return Ok()
+      }),
+    }),
+
+    "Check the intentions that were expired": step((ctx) => {
+      const { invoices, intentionsPending, currentDate } = ctx.data
+
+      const intentionsExpired = invoices
         .map((invoice) => {
-          const werePaid = invoice.is_confirmed
+          const wereExpired = invoice.expires_at > currentDate
 
-          if (werePaid) return intentionsPending.find((intention) => intention.invoiceId === invoice.id)
+          if (wereExpired) return intentionsPending.find((intention) => intention.invoiceId === invoice.id)
         })
         .filter(Boolean)
 
-      ctx.data.intentionsPaid = intentionsPaid
+      ctx.data.intentionsExpired = intentionsExpired
 
       return Ok()
     }),
 
-    "Changes the status of intents that have been paid": step(async (ctx) => {
-      const { intentionsPaid } = ctx.data
-      const { depositIntentionsDatabase } = ctx.di
+    "Changes the status of intents that have been expired": step(async (ctx) => {
+      const { intentionsExpired } = ctx.data
+      const { depositIntentionsInstance } = ctx.di
 
-      const intentionsUpdated = []
-
-      for (const intention of intentionsPaid) {
+      for (const intention of intentionsExpired) {
         try {
-          intention.status = depositStatusEnum.credited
+          intention.status = depositStatusEnum.expired
 
-          const updated = await depositIntentionsDatabase.update(intention)
-
-          intentionsUpdated.push(updated)
+          await depositIntentionsInstance.update(intention)
         } catch (error) {
           console.log(error)
         }
       }
 
-      ctx.data.intentionsUpdated = intentionsUpdated
-
-      return Ok()
-    }),
-
-    "Notifies users that the deposit has been credited": step(async (ctx) => {
-      const { bot } = ctx.di
-      const { intentionsUpdated } = ctx.data
-
-      for (const intention of intentionsUpdated) {
-        try {
-          const message = `Your deposit of ${intention.amount} has been credited`
-
-          await bot.telegram.sendMessage(intention.chatId, message)
-        } catch (error) {
-          console.log(`Error while try to send message to user ${intention.chatId}: ${error.message}`)
-        }
-      }
       return Ok()
     }),
   })
